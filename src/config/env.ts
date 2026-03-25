3db4df4962f48eb3c9f50d5e5d001b1ed63a7e5c
Ginotus <pe746807@gmail.com>
2026-03-25 16:34:12 +0700

    Create env.ts

---
+++ src/config/env.ts
@@ -0,0 +1,9 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const config = {
+  rpcUrl: process.env.RPC_URL!,
+  privateKey: process.env.PRIVATE_KEY!,
+  openaiKey: process.env.OPENAI_API_KEY!,
+};
